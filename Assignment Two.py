753ea11b97f09e353a3928366b8bb60aa27858c7
Benewaah Gloria <125013729+Benewaah-929991@users.noreply.github.com>
2023-02-22 14:20:53 +0000

    Add files via upload

---
+++ Assignment Two.py
@@ -0,0 +1,82 @@
+# @ autor Gloria Benewaah     - 6932321 
+# script for diffirent car brands and their respective prices in india
+# price in Rs Lakh 
+carPrice=float(input('input price of the car: ')) 
+if 10.48< carPrice <= 15.98:
+    print('Car Brand: Mahindra XUV 400EV')
+elif 15.98 < carPrice < 16.49:
+    print('Car Brand: Mahindra Thar')
+elif 10.45 < carPrice <= 10.48:
+    print('Car Brand: Urban Cruiser Hyryder')
+elif 7.80 < carPrice <=8.19:
+    print('Car Brand: Maruti Brezza')
+elif 8.19 < carPrice <= 8.35:
+    print('Car Brand: Maruti Ertiga')
+elif 8.35 < carPrice <=8.69:
+    print('Car Brand: Tata Tiago')
+elif 8.69 < carPrice <= 9.63:
+    print('Car Brand: Hyundai Verna')
+elif 9.63 < carPrice < 9.99:
+    print('Car Brand:Tata Nexon')
+elif 9.99 < carPrice <=10.45:
+    print('Car Brand: Grand Vitara')
+elif 6.00 < carPrice <= 6.29:
+    print('Car Brand: Hyundai Aura')
+elif 6.29 < carPrice <= 6.33:
+    print('Car Brand: Renault Triber')
+elif 6.33 < carPrice <= 6.44:
+    print('Car Brand: Maruti Dzire')
+elif 6.44 < carPrice <= 6.49:
+    print('Car Brand: Renault Kiger')
+elif 6.49 < carPrice <= 6.56:
+    print('Car Brand: Baleno')
+elif 6.56 < carPrice <= 6.66:
+    print('Car Brand: Glanza')
+elif 6.66 < carPrice < 7.80:
+    print('Car Brand: Tata Punch')
+elif 32.59 < carPrice <= 45.90:
+    print('Car Brand: BMW X1')
+elif 45.90 < carPrice <= 50.34:
+    print('Car Brand: Toyota Fortuner')
+elif 50.34 < carPrice <= 50.50:
+    print('Car Brand: Mercedes-Benz GLA')
+elif 50.50 < carPrice <= 51.43:
+    print('Car Brand: Audi Q3 Sportback')
+elif 60.95 <= carPrice < 61.60:
+    print('Car Brand: Kia EV6')
+elif 61.60 < carPrice <= 66.26:
+    print('Car Brand: Audi A6')
+elif 79.90 < carPrice <= 84.90:
+    print('Car Brand: BMW i7')
+elif 84.90 < carPrice <= 89.41:
+    print('Car Brand: Range Rover Velar')
+elif 89.41 < carPrice <= 96.50:
+    print('Car Brand: Volvo XC90')
+elif 16.49 < carPrice <= 18.68:
+    print('Car Brand: Hyundai Creta')
+elif 18.68 < carPrice <= 23.84:
+    print(' Car Brand: Hyundai Kona Electric')
+elif 899.00 < carPrice <= 900.00: 
+    print('Car Brand: Lamborghini Aven')
+elif 328.99 < carPrice <= 329.00:
+    print('Car Brand: Bentley Continental')
+elif 96.50 < carPrice <= 98.50:
+    print('Car Brand: BMW X5') 
+elif 3.54 < carPrice <= 3.60:
+    print('Car Brand: Bajaj Qute (RE60)')
+elif 3.00 < carPrice <= 3.54: 
+    print('Car Brand: Maruti Alto 800')
+elif 3.54 < carPrice <= 3.99:
+    print('Car Brand: Maruti Alto K10')
+elif 160.00 < carPrice <=174.00:
+    print('Car Brand: Mercedes-Benz S-class')
+elif 195.00 < carPrice <= 257.00: 
+    print('Car Brand: Porsche Cayenne')
+elif 567.99 < carPrice <= 568.00:
+    print('Car Brand:Hyundai Grand i10 Nios')
+elif 599.99 < carPrice <= 600.00:
+    print('Bentley Bentayga EWB')
+    
+
+    
+    
